5aa1987c954e52b098847e78fa9b8aac8b47b5d4
arun <arun@supplynote.in>
2025-03-26 20:22:54 +0530

    column for embeddings added in document model

--- app/db/models.py
+++ app/db/models.py
@@ -1,5 +1,4 @@
-from sqlalchemy import Column, String, Text, Enum, DateTime, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, String, Text, Enum, DateTime, LargeBinary
 from datetime import datetime, timezone
 from app.db.database import Base
 import enum
@@ -11,9 +10,10 @@ class IngestionStatus(str, enum.Enum):
     FAILED = "Failed"
 
 class Document(Base):
-    __tablename__ = "document"
+    __tablename__ = "documents"
 
     id = Column(String, primary_key=True, index=True)
     content = Column(Text, nullable=False)
     status = Column(Enum(IngestionStatus), default=IngestionStatus.PENDING)
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
+    embedding = Column(LargeBinary, nullable=True)  # Stores serialized embeddings
