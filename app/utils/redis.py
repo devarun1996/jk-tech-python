fb211ba451f441c80fd3b61fb34304ff7881c2ca
arun <arun@supplynote.in>
2025-04-01 13:46:09 +0530

    redis env variable used

--- app/utils/redis.py
+++ app/utils/redis.py
@@ -1,7 +1,11 @@
 import redis
+import os
+
+redis_host = os.getenv("REDIS_HOST", "localhost")
+redis_port = os.getenv("REDIS_PORT", 6379)
 
 # Initialize Redis client
-redis_client = redis.Redis(host="localhost", port=6379, db=0, decode_responses=True)
+redis_client = redis.Redis(host=redis_host, port=redis_port, db=0, decode_responses=True)
 
 # Test the connection
 try:
