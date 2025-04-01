c7da14e5db3483ba2eb4ef1f4bebc9ad654ffeac
arun <arun@supplynote.in>
2025-04-01 13:47:39 +0530

    env variable used,  import path updated for "declarative_base"

--- app/db/database.py
+++ app/db/database.py
@@ -1,10 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 import os
+from dotenv import load_dotenv
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://arun:postgres@localhost:5432/document_ingestion")
+# Load .env file
+load_dotenv()
 
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
